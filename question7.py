5737e9fc0a6333680923154e1525f7e89d0a1d93
Sahithi1010 <92219333+Sahithi1010@users.noreply.github.com>
2024-01-29 23:11:23 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,13 +1,13 @@
 def question7_1():
-    answer = 'non-increasing'
-    return answer
-
-def question7_2():
     answer = 'increase/decrease'
     return answer
 
+def question7_2():
+    answer = 'non-decreasing'
+    return answer
+
 def question7_3():
-    answer = [(-infinity, 2), (2, 3), (3, 5), (5, 'infinity')]
+    answer = [(0., 4.), (4.,5.), (5.,8.), (8., 'infinity')]
     #The representation of inifinity is 'infinity'. 
     #bi, ei are floats or 'infinity'. 
     return answer 
