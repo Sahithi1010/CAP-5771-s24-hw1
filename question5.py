b85eb23d80a2a2af66717f80b90b3bcdca17e51a
Sahithi1010 <92219333+Sahithi1010@users.noreply.github.com>
2024-01-29 20:50:07 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,32 +1,26 @@
 def question5_1():
-    """
     answer = {
             'bin1': [1,2]
             'bin2': [3,4,5,6],
             'bin3': [7,8,9]
     }
     where di, ei, fi are integers. 
-    """
     return answer
 
 def question5_2():
-    """
     answer = {
             'bin1': [1,2,3]
             'bin2': [4,5,6],
             'bin3': [7,8,9]
     }
     where di, ei, fi are integers. 
-    """
     return answer
 
 def question5_3():
-    """
     answer = {
             'bin1': [1,5,6]
             'bin2': [2,3,4],
             'bin3': [7,8,9]
     }
     where di, ei, fi are integers. 
-    """
     return answer 
