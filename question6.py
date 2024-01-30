8ccad4908c0acfd784fad90cb1edccf77a253763
Sahithi1010 <92219333+Sahithi1010@users.noreply.github.com>
2024-01-29 23:09:58 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,14 +1,14 @@
 def question6_1():
     answer = {
-        'equal_width': ['Change', 0],
-        'equal_frequency': ['Change', 0]
+        'equal_width': ['No Change',3],
+        'equal_frequency': ['No Change', 3]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['Change', 2]
+        'equal_width': ['No Change', 7],
+        'equal_frequency': ['No Change', 9]
     }
     return answer
 
